a97ac7ea8158c45ef345a23b6ba5f27435c27d4f
WashingtonSG <washington.gacho@dcomp.ufs.br>
2022-04-28 20:56:13 -0300

    database connection

--- resources/data/usersData.js
+++ resources/data/usersData.js
@@ -1,20 +1,36 @@
-const database = require('./')
+import database from './database'
+const { Pool } = require('pg')
 
 exports.getUsers = function () {
-  return database.query('SELECT * FROM alpha_profiler.users')
+  const pool = new Pool(database)
+  pool.connect()
+  return pool.query('SELECT * FROM alpha_profiler.users')
+    .then(pool.end)
 }
 exports.getUser = function (id) {
-  return database.query('SELECT * FROM alpha_profiler.users WHERE id=$1', id)
+  const pool = new Pool(database)
+  pool.connect()
+  return pool.query('SELECT * FROM alpha_profiler.users WHERE id=$1', id)
+    .then(pool.end)
 }
 exports.saveUser = function (user) {
-  return database.query(
+  const pool = new Pool(database)
+  pool.connect()
+  return pool.query(
     'INSERT INTO alpha_profiler.users(username, password,email,image,date_birth)' +
     'VALUES($1,$2,$3,$4,$5)', user)
+    .then(pool.end)
 }
 exports.deleteUser = function (id) {
-  return database.query('DELETE FROM alpha_profiler.users WHERE id=$1', id)
+  const pool = new Pool(database)
+  pool.connect()
+  return pool.query('DELETE FROM alpha_profiler.users WHERE id=$1', id)
+    .then(pool.end)
 }
 exports.updateUser = function (user) {
-  return database.query('UPDATE alpha_profiler.users' +
+  const pool = new Pool(database)
+  pool.connect()
+  return pool.query('UPDATE alpha_profiler.users' +
   'SET username=$1, password=$2, email=$3, image=$4, date_birth=$5', user)
+    .then(pool.end)
 }
