813b07dcb349c63953aa659617f46062ea794971
Anderson Ferreira Alves <andersonamericasul07@gmail.com>
2022-04-29 18:54:31 -0300

    feat: retira .env do versionamento

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const express = require('express')
 const userRouter = require('./routes/user')
-const { SERVER_PORT } = require('./utils/constants')
+const { SERVER_PORT, API_VERSION } = require('./utils/constants')
 
 const PORT = SERVER_PORT
 
 const app = express()
+
+app.get('/', (req, res) => {
+  res.send(`Api trabalho react versão: ${API_VERSION}!`)
+})
 
 app.use(cors())
 app.use(express.json())
